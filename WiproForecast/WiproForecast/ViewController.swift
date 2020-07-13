9d4e8b3d0239a135191a04c91d5769245c9616c7
Cris-Burlamaqui <crica.b@gmail.com>
2020-07-13 19:47:19 +0100

    Add files via upload
    First upload with first version of layout designed.

---
+++ WiproForecast/WiproForecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WiproForecast
+//
+//  Created by Cris Burlamaqui on 13/07/2020.
+//  Copyright © 2020 Cris Burlamaqui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
