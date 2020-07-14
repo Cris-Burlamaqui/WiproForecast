8143356916b9cef8a3d51c0f04cab7ce5bc084dd
Cris Burlamaqui <cris.burlamaqui@sonru.com>
2020-07-14 14:13:12 +0100

    Model initialisation adjustments.

--- WiproForecast/WiproForecast/Model/Forecast.swift
+++ WiproForecast/WiproForecast/Model/Forecast.swift
@@ -9,7 +9,18 @@
 import Foundation
 
 struct ForecastList: Codable {
+    
     let list: [Forecast]
+    
+    init(list: [Forecast]) {
+        var forecastArray: [Forecast] = []
+        
+        for forecast in list {
+            forecastArray.append(Forecast.init(main: forecast.main, weather: forecast.weather, dateString: forecast.dt_txt))
+        }
+        
+        self.list = forecastArray
+    }
 }
 
 struct Forecast: Codable {
@@ -17,4 +28,20 @@ struct Forecast: Codable {
     let main: Main
     let weather: [Weather]
     let dt_txt: String
+    
+    init(main: Main, weather: [Weather], dateString: String) {
+        self.main = main
+        self.weather = weather
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let date = dateFormatter.date(from: dateString)
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        if let d = date {
+            self.dt_txt = dateFormatter.string(from: d)
+        }
+        else {
+             self.dt_txt = dateString
+        }
+    }
 }
--- WiproForecast/WiproForecast/Model/Main.swift
+++ WiproForecast/WiproForecast/Model/Main.swift
@@ -14,4 +14,10 @@ struct Main: Codable {
     let temp_min: Double
     let temp_max: Double
     
+    init(temp: Double, tempMin: Double, tempMax: Double) {
+        self.temp = temp
+        self.temp_min = tempMin
+        self.temp_max = tempMax
+    }
+    
 }
