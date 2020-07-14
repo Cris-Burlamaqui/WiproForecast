904483fe33ee5f3be50e30e5831e6040bfb22c28
Cris Burlamaqui <cris.burlamaqui@sonru.com>
2020-07-14 14:15:39 +0100

    Business rule data processing.

--- WiproForecast/WiproForecast/ForecastViewController.swift
+++ WiproForecast/WiproForecast/ForecastViewController.swift
@@ -22,6 +22,8 @@ class ForecastViewController: UIViewController, UITableViewDelegate, UITableView
     
     @IBOutlet var forecastTableView: UITableView!
     
+    var forecastArrayList: [Forecast] = []
+    
     
     
     override func viewDidLoad() {
@@ -40,17 +42,69 @@ class ForecastViewController: UIViewController, UITableViewDelegate, UITableView
     
     
     
-    // MARK: Request delegate
+    // MARK: Request delegate - Load data
     
     func didRetrieveForecast(_ data: [Forecast]?) {
         if let forecastData = data {
             
             DispatchQueue.main.async {
-                self.forecastDescription.text = forecastData[0].weather[0].main
-                self.temperature.text = self.convertTemperature(forecastData[0].main.temp, from: .kelvin, to: .celsius)
-                self.forecastImage.image = self.convertImage(from: forecastData[0].weather[0].main)
+                
+                self.loadWeatherList(forecastData)
             }
         }
+    }
+    
+    func loadWeatherList(_ data:  [Forecast]) {
+        
+        var weekDays: [String: [Forecast]] = [:]
+        var weekArray = Array<Forecast>()
+        weekArray.append(data[0])
+        
+        for i in 1..<data.count {
+            
+            if data[i].dt_txt == data[i - 1].dt_txt {
+                weekArray.append(data[i])
+                continue
+            }
+            weekDays.updateValue(weekArray, forKey: data[i].dt_txt)
+            weekArray = []
+        }
+        
+        proccessWeatherListData(weekDays.sorted { $0.key < $1.key })
+        
+    }
+    
+    func proccessWeatherListData(_ forecastDictionary: [Dictionary<String, [Forecast]>.Element]) {
+        
+        for forecastList in forecastDictionary {
+            
+            let forecasts = forecastList.value as [Forecast]
+            
+            let mainList = forecasts.map { $0.main }
+            
+            let averageTemp = mainList.reduce(into: 0.0) { (result, main) in
+                result += main.temp
+            } / Double(mainList.count)
+            
+            let averageMinTemp = mainList.reduce(into: 0.0) { (result, main) in
+                result += main.temp_min
+            } / Double(mainList.count)
+            
+            let averageMaxTemp = mainList.reduce(into: 0.0) { (result, main) in
+                result += main.temp_max
+            } / Double(mainList.count)
+            
+            let main = Main.init(temp: averageTemp, tempMin: averageMinTemp, tempMax: averageMaxTemp)
+            let forecast = Forecast.init(main: main, weather: forecastList.value[0].weather, dateString: forecastList.key)
+            
+            self.forecastArrayList.append(forecast)
+        }
+        
+        self.forecastDescription.text = self.forecastArrayList[0].weather[0].main
+        self.temperature.text = self.convertTemperature(self.forecastArrayList[0].main.temp)
+        self.forecastImage.image = self.convertImage(from: forecastArrayList[0].weather[0].main)
+        
+        self.forecastTableView.reloadData()
     }
     
     
@@ -62,12 +116,19 @@ class ForecastViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        5
+        forecastArrayList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "forecastCell", for: indexPath) as! ForecastTableViewCell
+        
+        let forecast = forecastArrayList[indexPath.row]
+        cell.forecastIcon.image = convertImage(from: forecast.weather[0].main)
+        cell.weekDay.text = cell.getWeekDayText(from: forecast.dt_txt)
+        cell.minTemp.text = convertTemperature(forecast.main.temp_min)
+        cell.maxTemp.text = convertTemperature(forecast.main.temp_max)
+        cell.forecastDescription.text = forecast.weather[0].description
         
         return cell
     }
@@ -75,12 +136,12 @@ class ForecastViewController: UIViewController, UITableViewDelegate, UITableView
     
     // MARK: Convertions
     
-    func convertTemperature(_ temp: Double, from inputTempType: UnitTemperature, to outputTempType: UnitTemperature) -> String {
+    func convertTemperature(_ temp: Double, from inputTempType: UnitTemperature = .kelvin, to outputTempType: UnitTemperature = .celsius) -> String {
         let mf = MeasurementFormatter()
         mf.numberFormatter.maximumFractionDigits = 0
         mf.unitOptions = .providedUnit
-        let input = Measurement(value: temp, unit: UnitTemperature.kelvin)
-        let output = input.converted(to: UnitTemperature.celsius)
+        let input = Measurement(value: temp, unit: inputTempType)
+        let output = input.converted(to: outputTempType)
         return mf.string(from: output)
     }
     
